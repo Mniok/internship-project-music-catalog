f79f5f96bf5d768ac88ea0b921dc555699722189
Marian Stopyra <marianstopyraofficial@gmail.com>
2022-10-07 16:34:43 +0200

    add pinia, axios, bootstrap

--- remailVueFrontend/remailfront/src/main.ts
+++ remailVueFrontend/remailfront/src/main.ts
@@ -1,13 +1,24 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import './plugins/axios'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  pinia,
   render: h => h(App)
 }).$mount('#app')
