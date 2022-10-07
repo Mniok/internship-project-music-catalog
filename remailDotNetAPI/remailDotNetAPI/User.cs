c7aa561a24d98351e1523d14e63017437bd8049c
Marian Stopyra <marianstopyraofficial@gmail.com>
2022-10-07 17:45:31 +0200

    add basic auth model
    probably temporary, no database yet

---
+++ remailDotNetAPI/remailDotNetAPI/User.cs
@@ -0,0 +1,18 @@
+﻿namespace remailDotNetAPI
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Pass { get; set; } //probably hash or whatever, named pass for simplicity
+
+
+
+        //computed się robi tak: public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        //public string? Summary { get; set; } /// ? - nullable
+    }
+}
