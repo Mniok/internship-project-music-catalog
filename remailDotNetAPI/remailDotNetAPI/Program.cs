154f394cc38b69b08acce82d0e922462ef44d046
Marian Stopyra <marianstopyraofficial@gmail.com>
2022-10-11 16:42:35 +0200

    laid groundwork for auth using JwtBearer
    Added JwtBearer NuGet package to solution
    
    Added token object (JwtTokenConfig in JwtTokenConfig.cs)
    
    Added auth services through builder in Program.cs, and added JwtBearer support to that.

--- remailDotNetAPI/remailDotNetAPI/Program.cs
+++ remailDotNetAPI/remailDotNetAPI/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using remailDotNetAPI;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,55 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+/// add jwt
+var jwtTokenConfig = builder.Configuration.GetSection("jwtTokenConfig").Get<JwtTokenConfig>();      /// get settings for JwtBearer from appsettings.json jwtTokenConfig object
+builder.Services.AddSingleton(jwtTokenConfig);
+builder.Services.AddAuthentication(x =>                                     /// add auth here, using JwtBearer scheme
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>                                                        /// add JwtBearer for auth here, with settings
+{
+    x.RequireHttpsMetadata = true;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = jwtTokenConfig.Issuer,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtTokenConfig.Secret)),
+        ValidAudience = jwtTokenConfig.Audience,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.FromMinutes(1)
+    };
+});
+
+/*public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+{
+    var jwtConfig = configuration.GetSection("jwtConfig");
+    var secretKey = jwtConfig["secret"];
+    services.AddAuthentication(opt =>
+    {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtConfig["validIssuer"],
+            ValidAudience = jwtConfig["validAudience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+        };
+    });
+}*/
 
 var app = builder.Build();
 
@@ -18,6 +73,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();    //before UseAuth!
 app.UseAuthorization();
 
 app.MapControllers();
