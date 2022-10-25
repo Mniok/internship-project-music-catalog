37b6a5042800833580f1f6a8444955150f41e1dd
Marian Stopyra <marianstopyraofficial@gmail.com>
2022-10-25 17:18:15 +0200

    read, process, and display all data for chosen song
    when choosing song by id, display:
    -song name
    -list of artists
    -length (formatted)
    -list of genres
    -list of links
    -uploader and upload date

--- musicCatalogVueFrontend/src/views/SongView.vue
+++ musicCatalogVueFrontend/src/views/SongView.vue
@@ -1,5 +1,12 @@
 <template>
-  <div><p>song id {{$route.params.id}}: {{songTitle}} by {{artistsString}}</p></div>
+  <div>
+    <p>song id {{$route.params.id}}: {{songTitle}} by {{artistsString}}</p>
+    <p>{{songDescription}}</p>
+    <p>length: {{songTime | timeFormat}}</p>
+    <p>genres: {{genresString}}</p>
+    <p>links: {{linksString}}</p>
+    <p>uploaded by {{songUploader}} on {{songUploadedOn}}</p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,17 +14,28 @@
   import { useAccountStore } from '../store/account';
   import { mapState, mapActions } from 'pinia';
 
+  interface Link {
+    toSite: string,
+    linkBody: string
+  }
+
   export default Vue.extend({
     name: 'Song',
 
     data: () => ({
       song: '',
       songTitle: '',
+      songDescription: '',
+      songTime: 0,
       songArtists: Array<String>(),
+      songGenres: Array<String>(),
+      songLinks: Array<Link>(),
+      songUploader: '',
+      songUploadedOn: '',
     }),
 
     created() {
-      var loginRequest = axios.get('https://localhost:7026/api/Song/song/' + this.$route.params.id, {
+        axios.get('https://localhost:7026/api/Song/song/' + this.$route.params.id, {
           //id: this.$route.params.id
         },
         {
@@ -27,15 +45,30 @@
           console.log(response.data); ////
           this.song = response.data;
 
-          this.songTitle = response.data.title; 
+          this.songTitle = response.data.title;
+          this.songDescription = response.data.description;
+          this.songTime = response.data.time;
+          this.songUploader = response.data.uploadedBy.userName;
+          this.songUploadedOn = response.data.createdAt;
 
 
           for(var i=0; i<response.data.artists.$values.length; i++){
             this.songArtists.push(response.data.artists.$values.at(i).artistName);
           }
 
-          //this.songArtists.push("abcd");
-          console.log(this.songArtists);  ////
+          for(var i=0; i<response.data.genres.$values.length; i++){
+            this.songGenres.push(response.data.genres.$values.at(i).genreName);
+          }
+
+          for(var i=0; i<response.data.links.$values.length; i++){
+            var l : Link = {toSite: "", linkBody: ""};
+            l.toSite = response.data.links.$values.at(i).toSite;
+            l.linkBody = response.data.links.$values.at(i).linkBody;
+            this.songLinks.push(l);
+          }
+
+
+          //console.log(this.songArtists);  ////
 
 
         })
@@ -55,8 +88,36 @@
         return this.songArtists.join(", ");
       },
 
+      genresString(){
+        return this.songGenres.join(", ");
+      },
+
+      linksString(){
+        var links : Array<String> = ["(TEMPORARY before adding links is implemented): "];
+        this.songLinks.forEach(l => {
+          links.push( l.toSite + ".com/" + l.linkBody);
+        });
+        return links.join(", ");
+      },
+
       ...mapState(useAccountStore, ['accessToken', 'refreshToken']),
-    }
+    },
+
+    filters: {
+      timeFormat(value : number) {
+        //if (!value) return '';
+
+        var seconds : number = value % 60;
+        var minutes : number = ((value-seconds) % (60*60))/60;
+        var hours   : number = ((value -minutes*60 -seconds)) / (60*60);
+
+        if(hours > 0)
+          return `${hours}:${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')}`
+
+        return `${minutes}:${String(seconds).padStart(2,'0')}`;
+
+      }
+    },
 
   })
 </script>
